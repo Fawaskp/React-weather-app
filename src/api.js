6540cdeba306146669242cb8b72b8eb77ecbc4b6
Fawas <fawaskp1010@gmail.com>
2023-06-12 23:52:00 +0530

    first stage of weather app completed

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '055c1663a3msh04209c9a33836cfp1b6e97jsn4dfaf924ce1e',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+ 
+
+export const  WEATHER_API_KEY = '8ee6f69619c3f161fd78649f2a5b82df'
+export const  WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5'
